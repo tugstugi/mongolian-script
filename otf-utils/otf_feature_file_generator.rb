e9655ed5b7b859b752575c4b8f594b2cee6f7163
Erdene-Ochir Tuguldur <tugstugi@yahoo.com>
2010-09-21 00:15:07 +0200

    otf class and feature methods added...

--- otf-utils/otf_feature_file_generator.rb
+++ otf-utils/otf_feature_file_generator.rb
@@ -1,19 +1,78 @@
 require 'otf_feature_file_parser'
 
-def generate_group(group)
-  output = group.elements.map{|e| e.name}.join(" ");
-  if group.elements.size > 1
-    output = "[#{output}]"
+class OTFFeatureFile
+  def to_s
+    return classes.join + "\n\n" + features.join("\n\n")
   end
-  if group.replaceable?
-    output += "'"
-  end
-  return output
 end
 
-def generate_subtable(subtable)
-  output = "\t\tsub #{subtable.groups.map{|group| generate_group(group)}.join(" ")} by #{generate_group(subtable.replacedby)};\n"
-  return output
+class OTFFeature
+  def to_s
+    output = "feature #{name} {\n"
+    if script
+      output += "\tscript #{script};\n"
+    end
+    inner_features.each do |inner_feature|
+      output += "\tfeature #{inner_feature};\n"
+    end
+    if lookups.size == 1 && (lookups.first).name.eql?(name)
+      # inner subtables
+      if lookups.first.lookupflag
+        output += "\tlookupflag #{lookups.first.lookupflag};\n"
+      end
+      output += lookups.first.subtables.join
+      languages.each_key do |language|
+        output += "\tlanguage #{language};\n";
+      end
+    else
+      output += lookups.join
+      languages.each_key do |language|
+        output += "\tlanguage #{language} exclude_dflt;\n";
+        languages[language].each do |lookup|
+          output += "\t\tlookup #{lookup.name};\n"
+        end
+      end
+    end
+    output += "} #{name};\n"
+    return output
+  end
+end
+
+class OTFClass
+  def to_s
+    return "#{name}=[#{glyphs.map{|glyph| glyph.name}.join(" ")}];\n"
+  end
+end
+
+class OTFLookup
+  def to_s
+    output = "\tlookup #{name} {\n"
+    if lookupflag
+      output += "\t\tlookupflag #{lookupflag};\n"
+    end
+    output += subtables.join
+    output += "\t} #{name};\n"
+    return output
+  end
+end
+
+class OTFSubTable
+  def to_s
+    return "\t\tsub #{groups.map{|group| group.to_s}.join(" ")} by #{replacedby.to_s};\n"
+  end
+end
+
+class OTFGroup
+  def to_s
+    output = elements.map{|e| e.name}.join(" ");
+    if elements.size > 1
+      output = "[#{output}]"
+    end
+    if replaceable?
+      output += "'"
+    end
+    return output    
+  end
 end
 
 if ARGV.size == 0
@@ -24,61 +83,7 @@ end
 parser = OTFFeatureFileParser.new
 file = parser.parse_file(ARGV[0])
 if file
-  output = ''
-  
-  file.classes.each do |klass|
-    output += "#{klass.name}=[#{klass.glyphs.map{|glyph| glyph.name}.join(" ")}];\n"
-  end
-  
-  file.features.each do |feature|
-    output += "\n\nfeature #{feature.name} {\n"
-    
-    if feature.script
-      output += "\tscript #{feature.script};\n"
-    end
-    
-    feature.inner_features.each do |inner_feature|
-      output += "\tfeature #{inner_feature};\n"
-    end
-    
-    if feature.lookups.size == 1 && (feature.lookups.first).name.eql?(feature.name)
-      # inner subtables
-      
-      if feature.lookups.first.lookupflag
-        output += "\tlookupflag #{feature.lookups.first.lookupflag};\n"
-      end
-      feature.lookups.first.subtables.each do |subtable|
-        output += generate_subtable(subtable)
-      end
-      
-      feature.languages.each_key do |language|
-        output += "\tlanguage #{language};\n";
-      end
-    else
-      feature.lookups.each do |lookup|
-        output += "\tlookup #{lookup.name} {\n"
-        if lookup.lookupflag
-          output += "\t\tlookupflag #{lookup.lookupflag};\n"
-        end
-        lookup.subtables.each do |subtable|
-          output += generate_subtable(subtable)
-        end
-        output += "\t} #{lookup.name};\n"
-      end
-      
-      feature.languages.each_key do |language|
-        output += "\tlanguage #{language} exclude_dflt;\n";
-        feature.languages[language].each do |lookup|
-          output += "\t\tlookup #{lookup.name};\n"
-        end
-      end
-    end
-    
-    output += "} #{feature.name};\n\n"
-  end
-  
-  puts output
-  
+  puts file.to_s
 else
   puts "syntax error!"
 end
